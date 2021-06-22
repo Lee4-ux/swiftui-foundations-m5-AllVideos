8ba8082b604d9a73dd1f87688945ecf4244a8fa0
Lee4-ux <ljcob@protonmail.com>
2021-06-22 13:16:16 +0200

    Initial Commit

---
+++ AllVideos/AllVideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllVideosApp.swift
+//  AllVideos
+//
+//  Created by LJ Jacobs on 22/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct AllVideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
